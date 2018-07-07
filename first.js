f77c36ededaf107409af4932a4e02cfc6c099482
DevaHarsha44 <harshadeva44@gmail.com>
2018-07-07 16:25:34 +0530

    original

---
+++ first.js
@@ -0,0 +1,25 @@
+
+ function getFile(file,callback){
+ var xhr=new XMLHttpRequest();
+ xhr.overrideMimeType("application/json");
+ xhr.open("GET",file,true);
+  xhr.onreadystatechange=function(){
+   if(xhr.readyState===4 && xhr.status == "200")
+   {
+       callback(xhr.responseText);
+   }
+ };
+    xhr.send(null);
+ }
+  getFile("data.json",function(text){
+    var data=JSON.parse(text);
+    career(data.person);
+})
+function career(text)
+{
+  console.log(text);
+
+  document.getElementById("carobj").innerHTML="<h3>" +text.career.obj+ "</h3>"
+  document.getElementById("add").innerHTML="<h2>" +text.address.street+ " "+text.address.village+"</h2>"
+  document.getElementById("qual").innerHTML="<li>"+text.education.school.sname +" "+text.education.school.year +"</li>"+"<li>"+text.education.inter.cname +"</li>"+"<li>"+text.education.engineering.cname +"</li>"
+}
